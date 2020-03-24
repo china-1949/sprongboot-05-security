baf1fc33af4bf334a6a5164a8c4b2d028599e93f
“chenseng123 <your_regist1483202142@qq.com>
2020-03-24 12:58:59 +0800

    安全-权限控制&注销，使用默认的登录页面

---
+++ src/test/java/com/chens/security/Sprongboot05SecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chens.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sprongboot05SecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
